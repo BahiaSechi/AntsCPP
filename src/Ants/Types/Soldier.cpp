1fd85c3d7c05661fa9d0c1ed52b55dd0bdfd8d87
SECHI Bahia <bahia.sechi@ecole.ensicaen.fr>
2020-12-01 17:28:41 +0100

    quelques todo sur soldier et je  dois verifier que slave owner se deplace comme scout

--- src/Ants/Types/Soldier.cpp
+++ src/Ants/Types/Soldier.cpp
@@ -3,7 +3,6 @@
 //
 
 #include <Ants/Types/Soldier.h>
-#include <iostream>
 
 Soldier::Soldier(int expedition_time, const Position &position) :
 Ant(1, position, Alimentation(0.1,1)) {}
@@ -18,6 +17,11 @@ void Soldier::attack(SlaveOwner &slave_owner) const {
 
 void Soldier::move(Game *game)
 {
-    std::cout << "I'm a soldier" << std::endl;
-
+    //TODO Verifier pas de fourmi esclavagiste avant de bouger
+    //TODO Peut se deplacer si Tile decouverte
+    //TODO Rajouter boolean tile decouverte
+    //TODO A 100 jours, retour centre colonie
+    if (expedition_time == 100) {
+        //BLOU
+    }
 }

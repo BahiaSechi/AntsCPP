5f085ad9195d49d90363f88af821f5411607cd04
RABOTIN Matéo <mateo@rabotin.net>
2020-12-08 22:49:37 +0100

    Fixed rebase hell

--- src/Ants/Types/Scout.cpp
+++ src/Ants/Types/Scout.cpp
@@ -23,50 +23,58 @@ void Scout::play_turn(Game *game)
     /* Initialize variables. */
     int x_pos = this->position.getPos().x;
     int y_pos = this->position.getPos().y;
+    auto map = game->getMap();
+    int x_dimension = map->getDimension().x;
+    int y_dimension = map->getDimension().y;
+    int future_x, future_y;
 
     if (this->major) {
 
         switch (moving_chance) {
             case 1: // NO
-                this->position.setPos({x_pos-1,y_pos-1});
-                tileDiscovered(game, x_pos, y_pos);
+                future_x = x_pos-1;
+                future_y = y_pos-1;
                 std::cout << "Nord ouest" << std::endl;
                 break;
             case 2: // N
-                this->position.setPos({x_pos,y_pos-1});
-                tileDiscovered(game, x_pos, y_pos);
+                future_x = x_pos;
+                future_y = y_pos-1;
                 std::cout << "Nord" << std::endl;
                 break;
             case 3: // NE
-                this->position.setPos({x_pos+1,y_pos-1});
-                tileDiscovered(game, x_pos, y_pos);
+                future_x = x_pos+1;
+                future_y = y_pos-1;
                 std::cout << "Nord est" << std::endl;
                 break;
             case 4: // O
-                this->position.setPos({x_pos-1,y_pos});
-                tileDiscovered(game, x_pos, y_pos);
+                future_x = x_pos-1;
+                future_y = y_pos;
                 std::cout << "Ouest" << std::endl;
                 break;
             case 5: // E
-                this->position.setPos({x_pos+1, y_pos});
-                tileDiscovered(game, x_pos, y_pos);
+                future_x = x_pos+1;
+                future_y = y_pos;
                 std::cout << "Est" << std::endl;
                 break;
             case 6: // SO
-                this->position.setPos({x_pos-1,y_pos+1});
-                tileDiscovered(game, x_pos, y_pos);
+                future_x = x_pos-1;
+                future_y = y_pos+1;
                 std::cout << "Sud ouest" << std::endl;
                 break;
             case 7: // S
-                this->position.setPos({x_pos,y_pos+1});
-                tileDiscovered(game, x_pos, y_pos);
+                future_x = x_pos;
+                future_y = y_pos+1;
                 std::cout << "Sud" << std::endl;
                 break;
             case 8: // SE
-                this->position.setPos({x_pos+1,y_pos+1});
-                tileDiscovered(game, x_pos, y_pos);
+                future_x = x_pos+1;
+                future_y = y_pos+1;
                 std::cout << "Sud est" << std::endl;
                 break;
+        }
+        if (0 <= future_x && future_x < x_dimension && 0 <= future_y && future_y < y_dimension) {
+            this->position.setPos({future_x, future_y});
+            tileDiscovered(game, future_x, future_y);
         }
     }
 }

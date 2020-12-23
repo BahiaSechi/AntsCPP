c338f68905b0a0d7729bcf55799e798afa20fe7b
RABOTIN Matéo <mateo@rabotin.net>
2020-12-23 08:34:48 +0100

    Common copyrights accross files

--- src/Graphics/GameRender.cpp
+++ src/Graphics/GameRender.cpp
@@ -1,6 +1,22 @@
-//
-// Created by mateo on 12/12/2020.
-//
+/**
+ * Address :
+ * ENSICAEN
+ * 6 Boulevard Maréchal Juin
+ * F-14050 Caen Cedex
+ * Note :
+ * This file is owned by an ENSICAEN student.  No portion of this
+ * document may be reproduced, copied  or revised without written
+ * permission of the authors.
+ *
+ * @author Mateo RABOTIN <mateo.rabotin@ecole.ensicaen.fr>
+ * @author Bahia SECHI <bahia.sechi@ecole.ensicaen.fr>
+ *
+ * @date November 2020
+ * @file main.cpp
+ * @version 1.0
+ *
+ * @brief Blabla
+ */
 
 #include <Graphics/GameRender.h>
 
